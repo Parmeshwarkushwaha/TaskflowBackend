83ddf528caad4766ccb9717a4cdf1e2a08a2b21b
Parmeshwarkushwaha <kushwahaparmeshwar8@gmail.com>
2026-06-27 14:58:02 +0530

    Update project

--- server.js
+++ server.js
@@ -20,14 +20,21 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const allowedOrigins = [
   process.env.CLIENT_URL,
-  'http://localhost:3000',
-  'http://127.0.0.1:3000',
-].filter(Boolean);
+  "http://localhost:3000",
+];
 
-app.use(cors({
-  origin: allowedOrigins,
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 
